a5be16cffa60d4d3aba7fe0a74f8312ec3629b19
camila-rosas <camilarosas0628@gmail.com>
2026-04-20 03:18:27 -0500

    Update AccountController.java

--- backend/src/main/java/com/sms/backend/controller/AccountController.java
+++ backend/src/main/java/com/sms/backend/controller/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
     private final StudentService studentService;
     private final UserService userService;
