85d8f5d07adf097333d3b8bec1a6823f13d20098
camila-rosas <camilarosas0628@gmail.com>
2026-04-20 01:16:23 -0500

    Update dashboard.js

--- StudentPortal/dashboard.js
+++ StudentPortal/dashboard.js
@@ -14,3 +14,21 @@ axios.get("http://localhost:8080/api/students/dashboard", {
   document.querySelectorAll(".card-value")[2].innerText = "$" + data.balance;
 })
 .catch(err => console.error(err));
+
+document.addEventListener("DOMContentLoaded", () => {
+  const token = localStorage.getItem("token");
+
+  axios.get("http://localhost:8080/api/students/dashboard", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then(res => {
+    const data = res.data;
+
+    document.querySelectorAll(".card-value")[0].innerText = data.courses;
+    document.querySelectorAll(".card-value")[1].innerText = data.credits;
+    document.querySelectorAll(".card-value")[2].innerText = "$" + data.balance;
+  })
+  .catch(err => console.error(err));
+});
