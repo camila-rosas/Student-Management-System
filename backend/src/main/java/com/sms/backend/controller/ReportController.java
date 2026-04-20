3c354232f7b17f4caca39f22945b97dfdb767090
camila-rosas <camilarosas0628@gmail.com>
2026-04-20 03:18:45 -0500

    Update ReportController.java

--- backend/src/main/java/com/sms/backend/controller/ReportController.java
+++ backend/src/main/java/com/sms/backend/controller/ReportController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/reports")
+@RequestMapping("/api/reports")
 public class ReportController {
     @Autowired
     RegistrationRepository registrationRepository;
